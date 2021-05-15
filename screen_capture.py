8bbb212fef1044624c4064d7a36776ecfa92aadb
Haiserest <blach2542@hotmail.com>
2021-05-15 17:33:31 +0700

    attemp

--- screen_capture.py
+++
@@ -1,6 +0,0 @@
-import pyautogui
-import datetime
-
-x = str(datetime.datetime.now().strftime("%d-%b-%Y %H-%M-%S"))
-print(x)
-pyautogui.screenshot().save('C:/Users/asus/Documents/Project file/Temp/attemp1/'+ x +'.jpg')
